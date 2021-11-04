90bf48de10c798106c5ad58e64e99480100a7963
juanjo255 <piconcossio@gmail.com>
2021-11-04 11:32:43 -0500

    error conectando con base de datos

--- db/db.js
+++ db/db.js
@@ -4,7 +4,10 @@ import dotenv from "dotenv";
 dotenv.config({path: "./.env"})
 
 const stringDeConexion = process.env.DATABASE_URL;
-const Client  = new MongoClient (stringDeConexion, {useNewUrlParser:true, useUnifiedTopology:true})
+const Client  = new MongoClient (stringDeConexion, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 let conexionBaseDeDatos;
 
@@ -12,7 +15,7 @@ const conectarBD = (callback) => {
     Client.connect((err, db) =>{
         if (err){
             console.error("error conectando a la base de datos");
-            return false;
+            return "error";
         }
         conexionBaseDeDatos = db.db("beers");
         console.log("conexion exitosa")
