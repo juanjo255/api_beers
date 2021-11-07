f06176891504fbdd24bd15c4183059e494a7fe22
juanjo255 <piconcossio@gmail.com>
2021-11-07 17:01:58 -0500

    se agregaron 2 dependencias express-jwt y jwks-rsa, tocó por separado

--- server.js
+++ server.js
@@ -5,9 +5,24 @@ import Cors from "cors";
 import { conectarBD } from "./db/db.js";
 import rutasBeers from "./view/beers/rutas.js";
 import rutasEmployees from "./view/employees/rutas.js";
+import jwt from 'express-jwt';
+import jwks from "jwks-rsa";
 
-const app = Express() 
+const app = Express()
 
+var jwtCheck = jwt({
+    secret: jwks.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: 'https://cervezas-proyecto.us.auth0.com/.well-known/jwks.json'
+    }),
+    audience: 'https://api-cerverceria-autenticacion/',
+    issuer: 'https://cervezas-proyecto.us.auth0.com/',
+    algorithms: ['RS256']
+});
+
+app.use (jwtCheck)
 app.use (Express.json());
 app.use (Cors());
 app.use (rutasBeers);
